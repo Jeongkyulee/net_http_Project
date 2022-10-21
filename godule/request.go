9e2a347a25a5731350437708336760377139e0eb
leejeongkyu <jeongkyu1145@gmail.com>
2022-10-21 09:16:10 +0900

    commit
    commit

---
+++ godule/request.go
@@ -0,0 +1,10 @@
+package godule
+
+import (
+	"fmt"
+)
+
+func CheckVariable(s string) string {
+	fmt.Println(s)
+	return s
+}
--- main.go
+++ main.go
@@ -1,19 +1,49 @@
-package net_http_Project
+package main
 
 import (
+	"fmt"
 	"net/http"
+	"net_http_Project/godule"
 )
 
 func main() {
-	initconst()
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte("Hello, world!")) // 웹 브라우저에 응답
-	}) // / 경로에 접속했을 때 실행할 함수 설정
+	godule.CheckVariable("hello")
+	s := "Hello, world!"
 
-	http.ListenAndServe(":80", nil) // 80번 포트에서 웹 서버 실행
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		// HTML로 웹 페이지 작성
+		html := `
+		<html>
+		<head>
+			<title>Hello</title>
+		</head>
+		<body>
+			<span class="hello">` + s + `</span>
+		</body>
+		</html>
+		`
+
+		res.Header().Set("Content-Type", "text/html") // HTML 헤더 설정
+		res.Write([]byte(html))                       // 웹 브라우저에 응답
+	})
+
+	http.Handle( // /assets/ 경로에 접근했을 때
+		// 파일 서버를 동작시킴
+		"/assets/",
+		http.StripPrefix( // 파일 서버를 실행할 때 assets
+			// 디렉터리를 지정했으므로 URL 경로에서 /assets/ 삭제
+			"/assets/",
+			http.FileServer(http.Dir("assets")), // 웹 서버에서 assets
+			// 디렉터리 아래의 파일을 표시
+		),
+	)
+	defer func() {
+		http.ListenAndServe(":80", nil) // 80번 포트에서 웹 서버 실행
+	}()
 }
 
-func nethttp_CanonicalHeaderKey() {
-	//http.CanonicalHeaderKey()
-
+func CheckErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
